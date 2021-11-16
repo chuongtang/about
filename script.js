595cd3c387da4e048095d28d249feeaf0fa6e4f5
chuongtang <chuong.tang@aol.com>
2021-11-16 14:34:15 -0700

    Completed V3 of my portfolio

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 const projectList = [
     'UnsplashAPI-InfiniteScroll', 
     'JokeMachine',
@@ -12,4 +14,16 @@ const liveName = 'https://chuongtang.github.io/';
 function openLinktoNewPage (item, index){
     window.open (`${item}${projectList[index]}`); 
 }
-
+const openPopover =(event,popoverID)=> {
+    let element = event.target;
+    while(element.nodeName !== "BUTTON"){
+      element = element.parentNode;
+    }
+    let popper = Popper.createPopper(element, document.getElementById(popoverID), {
+      placement: 'top'
+    });
+    console.log("poper", popoverID);
+    
+    document.getElementById(popoverID).classList.toggle("hidden");
+    
+  }
